bcfc854037b3a8ebe30843536beb05be4466b971
Rachel-Imali-Kenyani <rachel.kenyani@pixeledge.io>
2024-11-18 10:11:53 +0300

    Added the footer component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import React from "react";
+import { Footer } from "../components";
 
 export const metadata: Metadata = {
   title: "RCL Rentals Informational Website",
@@ -14,7 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased">{children}</body>
+      <body>
+        <main className="flex-grow">{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
